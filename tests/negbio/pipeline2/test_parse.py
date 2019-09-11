9a31194d0571a67cf6fb16059b96ca354af9cead
Yifan Peng <yifan.peng@nih.gov>
2019-09-11 12:06:01 -0400

    add test cases for dner chexpert

--- tests/negbio/pipeline2/test_parse.py
+++ tests/negbio/pipeline2/test_parse.py
@@ -9,34 +9,34 @@ from tests.negbio.utils import text_to_bioc
 parser = NegBioParser()
 
 
-class TestNegBioParser:
-    def test_parse(self):
-        text = 'No pneumothorax.'
-        tree = '(S1 (S (S (NP (DT No) (NN pneumothorax))) (. .)))'
-        t = parser.parse(text)
-        assert str(t) == tree, str(t)
+def test_parse():
+    text = 'No pneumothorax.'
+    tree = '(S1 (S (S (NP (DT No) (NN pneumothorax))) (. .)))'
+    t = parser.parse(text)
+    assert str(t) == tree, str(t)
 
+    with pytest.raises(ValueError):
+        parser.parse('')
+
+    with pytest.raises(ValueError):
+        parser.parse('\n')
+
+    if sys.version_info[0] == 2:
         with pytest.raises(ValueError):
-            parser.parse('')
+            parser.parse(u'\xe6')
+    else:
+        t = parser.parse(u'\xe6')
+        assert str(t) == u'(S1 (S (NP (NN \xe6))))'
 
-        with pytest.raises(ValueError):
-            parser.parse('\n')
 
-        if sys.version_info[0] == 2:
-            with pytest.raises(ValueError):
-                parser.parse(u'\xe6')
-        else:
-            t = parser.parse(u'\xe6')
-            assert str(t) == u'(S1 (S (NP (NN \xe6))))'
+def test_parse_doc():
+    text = 'No pneumothorax.'
+    tree = '(S1 (S (S (NP (DT No) (NN pneumothorax))) (. .)))'
+    document = text_to_bioc([text], type='d/p/s')
+    d = parser.__call__(document)
+    assert d.passages[0].sentences[0].infons['parse tree'] == tree
 
-    def test_parse_doc(self):
-        text = 'No pneumothorax.'
-        tree = '(S1 (S (S (NP (DT No) (NN pneumothorax))) (. .)))'
-        document = text_to_bioc([text], type='d/p/s')
-        d = parser.__call__(document)
-        assert d.passages[0].sentences[0].infons['parse tree'] == tree
-
-        # test empty sentence
-        document = text_to_bioc([''], type='d/p/s')
-        d = parser.__call__(document)
-        assert d.passages[0].sentences[0].infons['parse tree'] is None
+    # test empty sentence
+    document = text_to_bioc([''], type='d/p/s')
+    d = parser.__call__(document)
+    assert d.passages[0].sentences[0].infons['parse tree'] is None
